11f34c5e97c405b4dbad0ad1ffd166d25dcc7d4a
Alexandru Stefan Andries <20018146@studenti.uniupo.it>
2020-05-20 16:08:20 +0200

    Aggiornato info panel

--- assets/script.js
+++ assets/script.js
@@ -8,7 +8,9 @@ var text ={
 			"summary":'If you want to talk with me, you can find here : <div id="contactList"><br><a href="https://github.com/RasterCrow" target="_blank"><img class="link_icon" src="assets/data/github-icon.png" alt="github icon"> Github</a> <br> <a href="https://it.linkedin.com/in/alex-andries" target="_blank"><img class="link_icon" src="assets/data/Linkedin-Icon.png" alt="linkedin icon"> LinkedIn</a> <br> <a href="https://steamcommunity.com/id/rastercrow" target="_blank"><img class="link_icon" src="assets/data/steam-icon.png" alt="steam icon"> Steam</a>  <br> <a href="https://trakt.tv/users/rastercrow " target="_blank"><img class="link_icon" src="assets/data/trakt-icon.png" alt="trakt icon"> Trakt </a> <br> <a href="https://twitter.com/rastercrow" target="_blank"><img class="link_icon" src="assets/data/twitter-icon.png" alt="twitter icon"> Twitter</a> <br> <a href="mailto: rastercrow@gmail.com"><img class="link_icon" src="assets/data/mail-icon.png" alt="mail icon"> Mail</a> ( rastercrow@gmail.com )</div>'
 		},
 		"blogAlert":"Unfortunately, most of the blog is and will be written in Italian... sorry international people.",
-		"info":"Hi, I'm Alex, better known as RasterCrow on the web. <br> I like to code, play games and watch movies. \n ", 
+		"info":"Hi, I'm Alex, better known as RasterCrow on the web. <br> I like to code, play games and watch movies. ", 
+		"infoExpandButton" : "More..",
+		"infoExpanded" : "<br>Passionate about all that is digital.<br><br>In my spare time I delight in projects in many fields; from the development of Android applications (Studio / Flutter) to the creation and development of simple video games in Unity, UE or other indie engines.<br><br>I am interested in the field of artificial intelligence. I had the opportunity to work with services such as Tensorflow and IBM Power AI, for the training of neural networks to be used in object detection services.<br>Thanks to this knowledge I have also implemented various services with the use of a Raspberry in the home automation sector (Home Surveillance, etc. ..).<br><br>I avidly follow the PC hardware sector, both at the market level (always updated with news from the big manufacturers), and at a personal level. I work with the hardware, going from the assembly and management of simple PCs, to the more enthusiast side of it with overclocking and more high-end hardware.<br><br>I also follow with interest cloud services, mainly AWS. I have worked with products like S3 and Lambda, with which I have published several Skills for Alexa devices.<br><br>Big fan of videogames; I actively follow the market and newest technologies. ",
 		"portfolio":{
 			"summary":'Some of the projects I worked on in my free time. <br> You can find more of them on <a href="https://github.com/RasterCrow" target="_blank"><img class="link_icon" src="assets/data/github-icon.png" alt="github icon"> Github</a> or on my Blog.',
 			"slide1":{
@@ -31,7 +33,9 @@ var text ={
 			"summary":'Se vuoi parlare con me puoi trovarmi su queste piattaforme : <div id="contactList"><br><a href="https://github.com/RasterCrow" target="_blank"><img class="link_icon" src="assets/data/github-icon.png" alt="github icon"> Github</a> <br> <a href="https://it.linkedin.com/in/alex-andries" target="_blank"><img class="link_icon" src="assets/data/Linkedin-Icon.png" alt="linkedin icon"> LinkedIn</a> <br> <a href="https://steamcommunity.com/id/rastercrow" target="_blank"><img class="link_icon" src="assets/data/steam-icon.png" alt="steam icon"> Steam</a>  <br> <a href="https://trakt.tv/users/rastercrow " target="_blank"><img class="link_icon" src="assets/data/trakt-icon.png" alt="trakt icon"> Trakt </a> <br> <a href="https://twitter.com/rastercrow" target="_blank"><img class="link_icon" src="assets/data/twitter-icon.png" alt="twitter icon"> Twitter</a> <br> <a href="mailto: rastercrow@gmail.com"><img class="link_icon" src="assets/data/mail-icon.png" alt="mail icon"> Mail</a> ( rastercrow@gmail.com )</div>'
 		},
 		"blogAlert":"",
-		"info":"Ciao, mi chiamo Alex, ma sul web sono RasterCrow. <br> Programmo, videogioco e guardo molti film. \n ",
+		"info":"Ciao, mi chiamo Alex, ma sul web sono RasterCrow. <br> Programmo, videogioco e guardo molti film.",
+		"infoExpandButton" : "Espandi...",
+		"infoExpanded" : "<br>Appassionato di tutto ciò che è digitale.<br><br>Nel tempo libero mi diletto con progetti in molteplici campi; dallo sviluppo di applicazioni Android ( <b>Studio/Flutter</b> ) alla ideazione e sviluppo di semplici videogiochi in Unity, UE o altri engine indie.<br><br>Mi interessa il campo dell'intelligenza artificiale, ho avuto modo di lavorare con servizi come Tensorflow e IBM Power AI, per l'allenamento di reti neurali da usare nel campo dei servizi di object detection.<br>Grazie a queste conoscenze ho inoltro implementato diversi servizi con l'utilizzo di un Raspberry nel settore della domotica (Home Surveillance, ecc..).<br><br>Seguo avidamente il settore hardware PC, sia a livello di mercato (sempre aggiornato con le novità dei grandi produttori), che a livello personale. Lavoro con l'hardware delle macchine, a partire dal montaggio e gestione di semplici pc, per arrivare al lato più enthusiast con overclock e hardware più high-end. <br><br>Seguo inoltre con interesse ciò che riguarda i servizi cloud, principalmente AWS. Ho lavorato con prodotti tra cui S3 e Lambda, con i quali ho pubblicato diverse Skill per i dispositivi Alexa.<br><br>Per finire, sono un grande appassionato di videogiochi, ne seguo attivamente il mercato e le tecnologie di sviluppo.",
 		"portfolio":{
 			"summary":'Alcuni dei progetti su cui ho lavorato nel tempo libero. <br> Puoi trovarne altri sul mio profilo <a href="https://github.com/RasterCrow" target="_blank"><img class="link_icon" src="assets/data/github-icon.png" alt="github icon"> Github</a> o sul Blog.',
 			"slide1":{
@@ -74,7 +78,13 @@ $(document).ready(function() {
 	$(document).on('shown.bs.collapse', function(event){
         //console.log( "in! print e: " +event.type);
         event.target.scrollIntoView({ behavior: 'smooth', block: 'center' })
-    });
+	});
+	$("#moreinfoButton").on("click", function() {
+		document.getElementById("moreinfoButton").hidden=true;
+	})
+	
+	
+	
 	
 	//traduco inizialmente la pagina in italino
 	translatePage("it");
@@ -97,5 +107,7 @@ $(document).ready(function() {
 		$('#contactsTitle').text(text[language].contacts.title);
 		document.getElementById("contactsText").innerHTML = text[language].contacts.summary;
 		document.getElementById("blogAlert").innerHTML = text[language].blogAlert;
+		document.getElementById("moreinfoButton").innerHTML = text[language].infoExpandButton;
+		document.getElementById("moreInfoText").innerHTML = text[language].infoExpanded;
 	};
 });
